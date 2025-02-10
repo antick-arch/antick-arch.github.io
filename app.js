8a0d98b6b8848b1f6d2c8b4218fce617d49799b2
antick-arch <e241003@ugrad.iiuc.ac.bd>
2025-02-11 00:10:36 +0600

    Add catch-all route for handling 404 errors

--- app.js
+++ app.js
@@ -32,6 +32,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+// Catch-all route to handle 404 errors
+app.use((req, res, next) => {
+  res.status(404).send('404: NOT_FOUND');
+});
+
 dbConnection.then(() => {
     try {
         app.listen(port, () => {
